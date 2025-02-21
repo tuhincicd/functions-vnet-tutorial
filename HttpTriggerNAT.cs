69faddb08624c4f610484117c7a83fc3c003954e
Tuhin <Tuhin.Goswami@plooto.com>
2025-02-21 14:32:48 -0500

    update

---
+++ HttpTriggerNAT.cs
@@ -0,0 +1,17 @@
+#r "Newtonsoft.Json"
+
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
+using Newtonsoft.Json;
+
+public static async Task<IActionResult> Run(HttpRequest req, ILogger log)
+{
+    log.LogInformation("C# HTTP trigger function processed a request.");
+
+    var client = new HttpClient();
+    var response = await client.GetAsync(@"https://ifconfig.me");
+    var responseMessage = await response.Content.ReadAsStringAsync();
+
+    return new OkObjectResult(responseMessage);
+}
